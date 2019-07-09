0d7d73610b6e859bde661ee29da9509fcff4c439
Mike Chung <m.chung1128@gmail.com>
2019-07-09 15:55:57 -0400

    Add get request route to server

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
+const db = require('../database/index.js');
 
 // app.use(bodyParser.json());
 app.use(express.static('./client/dist'));
 
 // get request at specific end point
 app.get('/:product_id', (req, res) => {
-  res.send('hi');
+  // console.log(req.params.product_id);
+  let id = req.params.product_id;
+  db.getAllPhotos(id)
+    .then(urls => res.send(urls))
+    .catch(err => console.log(err))
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
