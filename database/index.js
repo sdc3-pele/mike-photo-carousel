9fb5ef5f93611285e980c85020e65f547fdee92f
Mike Chung <m.chung1128@gmail.com>
2019-07-09 12:35:25 -0400

    Move db pw to config file

--- database/index.js
+++ database/index.js
@@ -1,1 +1,8 @@
 const mysql = require('mysql');
+const mysqlConfig = require('./config.js');
+
+const db = mysql.createConnection(mysqlConfig);
+
+const getAllPhotos = () => {
+  let queryStr = 'select '
+}
--- database/seed.js
+++ database/seed.js
@@ -1,15 +1,7 @@
-// CREATE TABLE products (
-//   product_id int auto_increment,
-//   photo_urls JSON,
-//   primary key(product_id)
-// );
 const mockData = require('./mockData.js');
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-  user: 'root',
-  password: 'password',
-  database: 'fec_photos'
-});
+const mysqlConfig = require('./config.js');
+const connection = mysql.createConnection(mysqlConfig);
 
 connection.connect(err => {
   if (err) {
@@ -23,10 +15,11 @@ connection.connect(err => {
 const getRandomFrom = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)];
 };
-console.log(`TEST`, getRandomFrom(mockData))
+// console.log(`TEST`, getRandomFrom(mockData))
+
 const seed = (callback) => {
-  for (var i = 0; i < 100; i++) {
-    var queryStr = `insert into products(photo_urls) values ('${JSON.stringify(getRandomFrom(mockData))}');`
+  for (let i = 0; i < 100; i++) {
+    let queryStr = `insert into products(photo_urls) values ('${JSON.stringify(getRandomFrom(mockData))}');`
     connection.query(queryStr, (err, data) => {
       if (err) {
         callback(err);
