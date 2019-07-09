76079cc9db91f6469d7b56fe2ad4d7925fb43f81
Mike Chung <m.chung1128@gmail.com>
2019-07-09 15:56:35 -0400

    Add function to query db

--- database/index.js
+++ database/index.js
@@ -3,6 +3,19 @@ const mysqlConfig = require('./config.js');
 
 const db = mysql.createConnection(mysqlConfig);
 
-const getAllPhotos = () => {
-  let queryStr = 'select '
+
+const getAllPhotos = (id) => {
+  let queryStr = 'select * from products where product_id=?;'
+  let params = id;
+  return new Promise((resolve, reject) => {
+    db.query(queryStr, params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
+
+module.exports = {getAllPhotos};
