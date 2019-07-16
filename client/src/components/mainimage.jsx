6f9e8c90ea0ccc9cc06b2fba397bb09b83dbdf16
Mike Chung <m.chung1128@gmail.com>
2019-07-15 22:49:24 -0400

    Add styled component, change to class component, and add the thumbnail photo highlight on scroll functionality

--- client/src/components/mainimage.jsx
+++ client/src/components/mainimage.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const MainImage = ({ image }) => {
-  const images = JSON.parse(image.photo_urls);
-  return (
-    <div className="mainContainer">
-      {images.map(url => <img id={url} className="mainImages" src={url}></img>)}
-    </div>
-  );
-};
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
+const MainImages = styled.img`
+  height: 600px;
+  width: auto;
+`;
+
+class MainImage extends React.Component {
+  componentDidMount(props) {
+    const props1 = this.props;
+    const callback = function(entries) {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio === 1.0 && props1.currentPhoto !== entry.target.src && entry.isIntersecting) {
+          props1.setCurrentPhoto(entry.target.src);
+        }
+      });
+    };
+
+    const options = {
+      root: null,
+      rootMargin: '0%',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    const mainPics = window.document.getElementsByClassName('mainImages');
+
+    for (const picture of mainPics) {
+      observer.observe(picture);
+    }
+  }
+
+  render() {
+    const images = JSON.parse(this.props.image.photo_urls);
+
+    return (
+      <MainContainer>
+        {images.map((url) => <div><MainImages className="mainImages" id={url} src={url} ></MainImages></div>)}
+      </MainContainer>
+    );
+  }
+}
 
 export default MainImage;
