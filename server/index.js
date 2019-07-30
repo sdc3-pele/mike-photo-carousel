816be436efe79ce4e5497e940a65c105291f9d67
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-29 19:30:43 -0700

    complete getAll endpoint for cassandra

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {cassGetAll, cassPost, cassPut, cassDelete} = require('./controllers/photos.js');
 
 const app = express();
 const port = 3001;
@@ -8,24 +9,29 @@ app.use('/:id', express.static('./client/dist'));
 
 app.get('/api/photos/:id', (req, res) => {
   const { id } = req.params;
-  db.getAllPhotos(id)
-    .then(urls => res.send(urls).end())
-    .catch(err => console.log(err));
+  cassGetAll(id, (err, data) => {
+    if(err) console.log(err);
+    console.log(data);
+    res.send(data).end();
+  });
+  // db.getAllPhotos(id)
+  //   .then(urls => res.send(urls).end())
+  //   .catch(err => console.log(err));
 });
 
 
 app.post('/api/photos', (req, res) => {
-  // db post function
+  cassPost();
 });
 
 app.put('/api/photos/:id', (req, res) => {
   const { id } = req.params;
-  // db put function
+  cassPut(id);
 });
 
 app.delete('/api/photos/:id', (req, res) => {
   const { id } = req.params;
-  // db delete function
+  cassDelete(id);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
