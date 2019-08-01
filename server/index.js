e023fbd3e0695743926c3d48c69f8d4c43387892
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-01 16:05:19 -0700

    update CRUD endpoints to use postgres functions

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+require('newrelic');
 const express = require('express');
-const {cassGetAll, cassPost, cassPut, cassDelete} = require('./controllers/photos.js');
+
+//const {cassGetAll, cassPost, cassPut, cassDelete} = require('./controllers/photos.js');
+const {pgGetAll, pgPost, pgPut, pgDelete} = require('./controllers/photosPG.js');
 
 const app = express();
 const port = 3001;
@@ -9,29 +12,37 @@ app.use('/:id', express.static('./client/dist'));
 
 app.get('/api/photos/:id', (req, res) => {
   const { id } = req.params;
-  cassGetAll(id, (err, data) => {
+  pgGetAll(id, (err, data) => {
     if(err) console.log(err);
     console.log(data);
     res.send(data).end();
   });
-  // db.getAllPhotos(id)
-  //   .then(urls => res.send(urls).end())
-  //   .catch(err => console.log(err));
 });
 
 
 app.post('/api/photos', (req, res) => {
-  cassPost();
+  const value = req.query.val;
+  pgPost(value, (err, data) => {
+    console.log('data is: ' + data);
+    res.end();
+  });
 });
 
 app.put('/api/photos/:id', (req, res) => {
+  const value = req.query.val;
   const { id } = req.params;
-  cassPut(id);
+  pgPut(id, value, (err, data)=>{
+    console.log(err, data);
+    res.end();
+  });
 });
 
 app.delete('/api/photos/:id', (req, res) => {
   const { id } = req.params;
-  cassDelete(id);
+  pgDelete(id, (err, data) => {
+    console.log(err, data);
+    res.end()
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
